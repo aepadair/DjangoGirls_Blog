a47b770ea3aab0742ebd443ebcc8fbd6c9410c75
“Amy <amyepadair@gmail.com>
2018-06-16 16:36:12 +0100

    Created html templates, query sets, dynamic data in templates, modified templates to display posts from database

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 #asssinging view
 #so name= is the name of the url that will be used to identify the view
+
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 # Created a function (def) called post list, that takes request and returns a function 
 # that will render our template our template blog/post_list.html
+
+#we want published blog posts sorted by published date 
+# we get this piece of code inside by adding it to the function 'def post_list(request)'
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
